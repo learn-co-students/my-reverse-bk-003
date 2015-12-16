43d6206d97e3f6dee5dca999cc5fb0cc4aeb22d3
Andrew Persad <akpersad@gmail.com>
2015-12-16 09:31:43 -0500

    done

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,1 +1,12 @@
-# your code here
+class Array
+	def my_reverse
+		arr = []
+		while self.length != 0
+			arr << self.pop
+		end
+		while arr.length != 0
+			self << arr.shift
+		end
+		self
+	end
+end
