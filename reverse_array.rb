a94663aca4ee7d9bbb0a3448c281f34170564850
Stephen Koltz <steve.koltz@gmail.com>
2015-12-25 14:28:03 -0500

    reverse reverse

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,1 +1,14 @@
-# your code here
+require 'pry'
+class Array
+	def my_reverse
+		i = 0 
+		k = -1
+		(self.length/2).times do 
+			self[i], self[k] = self[k], self[i]
+			i += 1
+			k -= 1
+		end
+		self
+	end
+
+end
